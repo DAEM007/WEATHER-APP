debb6a5bc5e8e1a83a3129e69acc26675f1ca480
EMMANUEL <dammylola299@gmail.com>
2022-06-09 13:35:55 +0100

    revised UI images & icons

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,17 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon =document.querySelector('.icon img');
 
 
 const updateUI = (data) => {
-    const cityDets = data.cityDets;
-    const weather = data.weather;
+    // const cityDets = data.cityDets;
+    // const weather = data.weather;
+
+    // BY DESTRUCTURING THE ABOVE PROPERTIES WE THEN HAVE...
+
+    const { cityDets, weather } = data;
 
     // update the details to the template
     details.innerHTML = `
@@ -16,6 +22,23 @@ const updateUI = (data) => {
             <span>&deg;C</span>
         </div>
     `;
+
+    // Update night & day icon images
+    const iconSrc = `img/icons/${weather.weatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+    
+
+    let timeSrc = null;
+    if(weather.IsDayTime){
+        timeSrc = 'img/day.svg';
+    }else{
+        timeSrc = 'img/night.svg';
+    }
+
+    time.setAttribute('src', timeSrc);
+
+
+
 
     // remove the d-none class if present
     if(card.classList.contains("d-none")){
